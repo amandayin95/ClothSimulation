b93264f568bdea00df00c2fa08f10e9015b1d9f0
Amanda Yin <amandayin@amandas-macbook-pro.local>
2016-04-10 01:23:26 -0700

    Initial Commit

---
+++ ClothSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ClothSimulation
+//
+//  Created by Amanda Yin on 4/10/16.
+//  Copyright © 2016 Amanda Yin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
